02402d69226890e3b4f8c6c3bb97d92159513f25
GGS92carmnad <azk20heu@uea.ac.uk>
2022-04-05 14:44:59 +0100

    Just increased button horizontal padding

--- pms/src/components/Pages/LoginPage.js
+++ pms/src/components/Pages/LoginPage.js
@@ -10,8 +10,6 @@ function LoginPage() {
       <label htmlFor="password">Password</label>
 
       <RoundedButton colour="green" submit={true}>SUBMIT</RoundedButton>
-      <RoundedButton colour="green" href="/book">BOOK SPACE</RoundedButton>
-      <RoundedButton colour="green" onClick="myJavascriptFunction()">BOOK SPACE</RoundedButton>
     </form>
   );
 }
