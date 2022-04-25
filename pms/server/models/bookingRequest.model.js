afdbf572c48b66cb568553eed9fec85ec7313bf0
GGS92carmnad <azk20heu@uea.ac.uk>
2022-04-25 09:38:11 +0100

    Atomised booking into booking request model

--- pms/server/models/bookingRequest.model.js
+++ pms/server/models/bookingRequest.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  space_id: { type: String, required: true },
+  location: { type: String, required: true },
   start_timestamp: { type: Date, required: true },
   end_timestamp: { type: Date, required: true }
 });
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model('BookingRequest', bookingSchema);
